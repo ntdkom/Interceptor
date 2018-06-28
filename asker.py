abdf104184356052521811806268d8842880d9a7
Victor Gorshkov <39904075+ntdkom@users.noreply.github.com>
2018-06-28 16:35:22 +0300

    Changed phish hashes processing.

--- asker.py
+++ asker.py
@@ -14,15 +14,15 @@ try:
     with open('config.yml', 'r') as fconf:
         config = yaml.load(fconf)
         interval = config['phishpackets']['interval']
-        nbnsQuery = config['phishpackets']['NBNSquery']
-        llmnrQuery = config['phishpackets']['LLMNRquery']
+        nbns_query = config['phishpackets']['NBNSquery']
+        llmnr_query = config['phishpackets']['LLMNRquery']
         user = config['phishcredentials']['user']
         passwd = config['phishcredentials']['password']
         pc_name = config['phishcredentials']['workstation']
         pc_domain = config['phishcredentials']['domain']
-        targetNets = config['phishpackets']['destnets']
-        sniffFilter = config['sniffingconf']['filter']
-        localIp = config['sniffingconf']['localaddr']
+        target_nets = config['phishpackets']['destnets']
+        sniff_filter = config['sniffingconf']['filter']
+        local_ip = config['sniffingconf']['localaddr']
         webhook = config['webhook']
         log = config['logfile']
 except:
@@ -79,15 +79,16 @@ class Sniffer(Thread):
         print("[sniffer]: Packet received: {src} -> {dst}".format(src=ip_layer.src, dst=ip_layer.dst))
         if packet[UDP].dport == 137:
             post = {'text': "NBNS poisoning attempt: {data}".format(data=packet[UDP].summary())}
-            if self.smbuser:
-                print('[sniffer]: Trying to send phising hashes...')
-                try:
-                    conn = SMBConnection(self.smbuser, self.smbpass, self.smbcliname, self.smbsrvname, domain=self.smbdomain, use_ntlm_v2=True, is_direct_tcp=True)
-                    conn.connect(ip_layer.src, 445)
-                except:
-                    print('warning: Dummy hashes are sent, some further SMB hickup happened though.')
         elif packet[UDP].dport == 5355:
             post = {'text': "LLMNR poisoning attempt: {data}".format(data=packet[UDP].summary())}
+            # If SMB user option is specified in the config, let's try to sent phishing hashes by accessing the provided file server address
+            if self.smbuser:
+                print('[sniffer]: Trying to send phising hashes to file server: {0}'.format(packet[3].an.rdata))
+                try:
+                    conn = SMBConnection(self.smbuser, self.smbpass, self.smbcliname, self.smbsrvname, domain=self.smbdomain, use_ntlm_v2=True, is_direct_tcp=True)
+                    conn.connect(packet[3].an.rdata, 445)
+                except:
+                    print('[sniffer]: Something went wrong. It does not necessarily mean that hashes were not sent.')
         else:
             post = {'text': "Some unrecognized reply: {data}".format(data=packet[UDP].summary())}
         if self.webhook:
@@ -96,20 +97,20 @@ class Sniffer(Thread):
 
 # Main process
 print('[core]: Initializing Sniffer...')
-sniffer = Sniffer(filter=sniffFilter, webhook=webhook, smb_user=user, smb_client_name=pc_name, smb_srv_name=nbnsQuery, smb_domain=pc_domain, smb_pass=passwd)
+sniffer = Sniffer(filter=sniff_filter, webhook=webhook, smb_user=user, smb_client_name=pc_name, smb_srv_name=nbns_query, smb_domain=pc_domain, smb_pass=passwd)
 sniffer.start()
 sleep(10)
 # Will be sending phishing packets until Ctrl-C
 try:
     while True:
         print('[core]: Starting sending phish packets...')
-        for net in targetNets:
+        for net in target_nets:
             print('[asker]: sending to: {dst}'.format(dst=net))
-            queryid = random.getrandbits(16)
+            query_id = random.getrandbits(16)
             try:
-                send(IP(dst=net)/UDP(sport=137, dport="netbios_ns")/NBNSQueryRequest(SUFFIX="file server service",QUESTION_NAME=nbnsQuery, QUESTION_TYPE="NB"))
+                send(IP(dst=net)/UDP(sport=137, dport=137)/NBNSQueryRequest(SUFFIX="file server service", QUESTION_NAME=nbns_query, QUESTION_TYPE='NB'))
                 sleep(15)
-                send(IP(dst=net)/UDP(sport=5355, dport=5355)/LLMNRQuery(id=queryid, qr=0, opcode=0, qdcount=1, qd=DNSQR(qname=llmnrQuery,qtype='A')))
+                send(IP(dst=net)/UDP(sport=5355, dport=5355)/LLMNRQuery(id=query_id, qr=0, opcode=0, qdcount=1, qd=DNSQR(qname=llmnr_query,qtype='A')))
             except PermissionError as perr:
                 print('Cannot send packet, check you permissions: {info}'.format(info=perr))
         # Freeze for the specified interval and start all over again.
